3b40cafe37354ff701247ae8c5aeee90a8fc0ccc
Amazil <vip.noob.elf@gmail.ru>
2022-07-22 13:52:42 +0300

    two commit

--- cycl_two.js
+++ cycl_two.js
@@ -3,8 +3,8 @@ let arr = [ "a", "b", 3, "a", null, NaN, 'abrakadabra ', '3', 30, undefined,3,3,
 function count(arr) {
     let sum = 0
     for (i = 0; i < arr.length; i++) {
-        if (arr[i] == 3 ) {
-            console.log(arr[i]),sum++
+        if (arr[i] === "3" ) {
+            sum++
         }
     }
     return sum
