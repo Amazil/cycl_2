0936a6b0ff408727961a909c9b5dd4e17c1ac011
Amazil <vip.noob.elf@gmail.ru>
2022-07-22 13:43:48 +0300

    first_commit

---
+++ cycl_two.js
@@ -0,0 +1,14 @@
+let arr = [ "a", "b", 3, "a", null, NaN, 'abrakadabra ', '3', 30, undefined,3,3,"3",3,"3"]
+
+function count(arr) {
+    let sum = 0
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] == 3 ) {
+            console.log(arr[i]),sum++
+        }
+    }
+    return sum
+    
+}
+
+console.log(count(arr)) 
